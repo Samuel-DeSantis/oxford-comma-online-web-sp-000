10d3d1f81027922bf74f7a3c6ae6d7e50e0c41da
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-09-13 20:48:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@ def oxford_comma(array)
   when 2
     return "#{array[0]} and #{array[1]}"
   else
-    temp = array.slice(0, array.length - 2)
+    temp = array.slice(0, array.length - 1)
     temp_str = temp.join(", ")
     temp_str << "and #{array[array.length - 1]}"
     return temp_str
