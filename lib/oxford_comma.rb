ae8e0a76d6f07d281c60e9c81ca10d6b014d3e96
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-09-13 20:48:09 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,17 +1,14 @@
 def oxford_comma(array)
-  new_array = []
+  temp = []
   case array.length
   when 1
     return "#{array[0]}"
   when 2
     return "#{array[0]} and #{array[1]}"
   else
-    array.each do |item|
-      unless item === array[array.length-1]
-        new_array << "and #{item}"
-        next
-      end
-      new_array << "#{item}, "
-    end
+    temp = array.slice(0, array.length - 2)
+    temp_str = temp.join(", ")
+    temp_str << "and #{array[array.length - 1]}"
+    return temp_str
   end
 end
