f171540ab0b24f992668dad323a50e372ac9daf8
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-09-13 18:43:51 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  new_array = []
+  array.each do |item|
+    unless array[array.length] === item
+      new_array << "and #{item}"
+    end
+    new_array << "#{item}, "
+  end
 end
